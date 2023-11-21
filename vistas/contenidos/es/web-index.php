2c33d633f0c6791ecd9d5ae69fc850c3e1ada7c4
TulioHD12 <104809981+TulioHD12@users.noreply.github.com>
2023-11-21 08:43:10 -0700

    base

---
+++ vistas/contenidos/es/web-index.php
@@ -0,0 +1,9 @@
+<div class="banner">
+    <div class="banner-body">
+        <h3 class="text-uppercase">Bienvenido a <?php echo COMPANY; ?></h3>
+        <p>Los mejores productos y la mejor calidad los encuentras en <?php echo COMPANY; ?></p>
+        <p class="text-center">
+                <a href="<?php echo SERVERURL; ?>product/all" class="btn btn-primary" >Ver Servicios</a>
+            </p>
+    </div>
+</div>
